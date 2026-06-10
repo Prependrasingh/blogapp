8d85b606c165f81dc2a077a71dc55cfa0ccd9db1
prependra <prependrasingh23646knit@gmail.com>
2026-06-10 16:25:57 +0530

    cc

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,44 +1,46 @@
-const post = require("../models/postModel");
-
+const Post = require("../models/postModel");
 
 exports.createPost = async (req, res) => {
   try {
+    const { title, body } = req.body;
 
-    const {title , body} = req.body;
-    const Post = new post({
-        title , body
+    const newPost = await Post.create({
+      title,
+      body,
     });
 
-    const savedPost = await Post.save();
-
-    res.json({
-        post:savedPost,
-    })
-    
+    return res.status(201).json({
+      success: true,
+      post: newPost,
+      message: "Post created successfully",
+    });
   } catch (error) {
+    console.error(error);
 
-    return res.status(400).json({
-        error:"Error while creating post"
+    return res.status(500).json({
+      success: false,
+      error: "Error while creating post",
     });
-    
   }
-}
+};
 
-exports.getAllPosts = async (req , res) => {
-    try{
+exports.getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate("comments")
+      .sort({ _id: -1 });
 
-        const Posts = await post.find().populate("comments");
-        res.json({
-            Posts,
-        })
+    return res.status(200).json({
+      success: true,
+      count: posts.length,
+      Posts: posts,
+    });
+  } catch (error) {
+    console.error(error);
 
-    }
-    catch(error){
-
-        return res.status(400).json({
-            error: "Error while fetching posts",
-            
-        })
-
-    }
-}
+    return res.status(500).json({
+      success: false,
+      error: "Error while fetching posts",
+    });
+  }
+};

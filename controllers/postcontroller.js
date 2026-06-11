557bd75bc347e9e23ebcc919f457bdddb4733457
prependra <prependrasingh23646knit@gmail.com>
2026-06-11 12:01:00 +0530

    feat: add authorName to posts

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -2,11 +2,12 @@ const Post = require("../models/postModel");
 
 exports.createPost = async (req, res) => {
   try {
-    const { title, body } = req.body;
+    const { title, body , name} = req.body;
 
     const newPost = await Post.create({
       title,
       body,
+      name,
     });
 
     return res.status(201).json({
--- models/postModel.js
+++ models/postModel.js
@@ -1,22 +1,36 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  body: {
+    type: String,
+    required: true,
+  },
+
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Like",
     },
-    body: {
-        type: String,
-        required: true,
+  ],
+
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
     },
-    likes: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Like",
-    }],
-    comments: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Comment",
-    }]
+  ],
 });
 
 module.exports = mongoose.model("Post", postSchema);
